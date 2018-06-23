70ab086d301141282f4fd2acffdae4aa9a3151e7
MuhammadLahin <muhammad.lahin@gmail.com>
2018-06-23 16:57:43 +0600

    Update custom.js

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,12 +1,1 @@
-jQuery(document).ready(function(){
-	jQuery('.faq_title').on('click', function(e){
-		jQuery(this).parent().find('.faq_content').slideToggle(300);
-
-		jQuery(this).find(".faq_add_icon").toggle();
-		jQuery(this).find(".faq_minus_icon").toggle();
-
-		jQuery(this).find(".nf_simple_up_icon").toggle();
-		jQuery(this).find(".nf_simple_down_icon").toggle();
-
-	});
-});
+jQuery(document).ready(function(){jQuery(".faq_title").on("click",function(i){jQuery(this).parent().find(".faq_content").slideToggle(300),jQuery(this).find(".faq_add_icon").toggle(),jQuery(this).find(".faq_minus_icon").toggle(),jQuery(this).find(".nf_simple_up_icon").toggle(),jQuery(this).find(".nf_simple_down_icon").toggle()})});
